debb3c8722c60afded4e7a9ddddcf3f3608f42a0
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-05-06 18:14:53 -0400

    Implement 'listen' method to create a server where browsers can connect to

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
+// create a server where browsers can connect to.
+// We can do so with the help of a listen method
+// provided by Express:
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
 console.log('May the Node odds be ever in thy favor')
