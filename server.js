854022a4e455aa89873169dd22e57a830b988587
maria.inespeniche-vargas@generalassemb.ly <maria.inespeniche-vargas@generalassemb.ly>
2018-05-06 19:09:31 -0400

    Implement E6 syntax Fix unexpected token error Create & Require index.html to handle Express GET request

--- server.js
+++ server.js
@@ -7,4 +7,23 @@ const app = express();
 app.listen(3000, function() {
   console.log('listening on 3000')
 })
-console.log('May the Node odds be ever in thy favor')
+// console.log('May the Node odds be ever in thy favor')
+
+// PATHS
+
+// GET || READ
+
+// Note: request and response are usually written as
+// req and res respectively.
+// app.get('/', function (request, response) {
+//   // do something here
+//   res.send('Hello World')
+// })
+// ES6 syntax below:
+app.get('/', (req, res) => {
+  // res.send('Hi Mom!')
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript
+  // source code. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
